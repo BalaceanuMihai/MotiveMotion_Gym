1e312a52a7ddb188fc0086f42061f82bf17e333d
Bălăceanu Mihai <mihaib004@gmail.com>
2024-11-12 23:41:00 +0200

    Update test.php

--- MotiveMotion/test.php
+++ MotiveMotion/test.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'app/models/Membership.php';
 require_once 'app/models/User.php';
+require_once 'app/models/WorkoutPlan.php';
 
 $users = User::getAllUsers();
-
 echo "<p>Users:</p>";
 foreach ($users as $user) {
     echo "<pre>";
@@ -11,11 +11,19 @@ foreach ($users as $user) {
     echo "</pre>";
 }
 
-$memberships = Debt::getAllMemberships();
+$memberships = Membership::getAllMemberships();
 echo "<p>Memberships:</p>";
 foreach ($memberships as $membership) {
     echo "<pre>";
     print_r($membership);
+    echo "</pre>";
+}
+
+$workout_plans = WorkoutPlan::getAllWorkoutPlans();
+echo "<p>Workout Plans:</p>";
+foreach ($workout_plans as $workout_plan) {
+    echo "<pre>";
+    print_r($workout_plan);
     echo "</pre>";
 }
 
